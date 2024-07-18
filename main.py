df9775ce88ace04c1062b6d83a85c19b5ae11f1c
zlchtl <ebengrad1@mail.ru>
2024-07-18 14:58:22 +0300

    Добавить метод получения списка моделей и метод проверки статуса модели

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import json
 import requests
 import time
 
+
 class Text2ImageAPI:
-    def __init__(self, DEBUG=False):
-        self.DEBUG = DEBUG
+    def __init__(self, debug: bool = False):
+        self.DEBUG = debug
         self.init_loger()
         self.init_env()
         self.init_settings()
 
+        self.get_models()  # получение списка моделей
+        self.availability_service(self.models[0])  # проверка доступности модели
 
     def init_loger(self):
         self.logger = logging.getLogger(__name__)
@@ -27,10 +30,10 @@ class Text2ImageAPI:
         self.logger.debug(f"Инициализация логера")
 
     def init_env(self):
+        self.logger.debug(f"Инициализация виртуального окружения")
         load_dotenv()
         self.api_key = os.getenv("api_key")
         self.secret_key = os.getenv("secret_key")
-        self.logger.debug(f"Инициализация виртуального окружения: \n\tapi_key = {self.api_key}\n\tsecret_key = {self.secret_key}")
 
     def init_settings(self):
         with open('settings.json') as f:
@@ -39,14 +42,21 @@ class Text2ImageAPI:
         self.KEYS = settings['KEYS']
         self.AUTH_HEADERS = {
             'X-Key': f'Key {self.api_key}',
-            'X-Secret': f'Secret {self.secret_key}',
+            'X-Secret': f'Secret {self.secret_key}'
         }
         self.logger.debug(f"Инициализация настроек скрипта")
 
-    def availability_service(self):
-        response = requests.get(self.URL + self.KEYS["availability"], headers=self.AUTH_HEADERS)
+    def get_models(self):
+        self.logger.debug(f"Получение списка моделей")
+        response = requests.get(self.URL + self.KEYS["MODELS"], headers=self.AUTH_HEADERS)
+        self.models = response.json()
+
+    def availability_service(self, model: dict):
+        param = {'model_id': (None, model['id'])}
+        response = requests.get(self.URL + self.KEYS["availability"], headers=self.AUTH_HEADERS, files=param)
         data = response.json()
-        self.logger.debug(data)
+        self.logger.info(f'Статус модели <{model['name']} {model['version']}>: {data['status']}')
+
 
 if __name__ == "__main__":
-    generator = Text2ImageAPI(DEBUG=True)
+    generator = Text2ImageAPI(debug=True)
